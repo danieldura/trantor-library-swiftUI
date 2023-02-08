5cedfe06f47ac142aa9ed53033717baa204e18c5
Dani Durà <danieldura7@gmail.com>
2023-02-08 16:43:49 +0100

    Initial Commit

---
+++ Libreria Trantor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Libreria Trantor
+//
+//  Created by Dani Durà on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Libreria Trantor/Libreria_TrantorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Libreria_TrantorApp.swift
+//  Libreria Trantor
+//
+//  Created by Dani Durà on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Libreria_TrantorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
