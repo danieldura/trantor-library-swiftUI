949f9316aff7c34b13b870c33e0580c62b46f29d
Dani Durà <danieldura7@gmail.com>
2023-02-15 10:24:17 +0100

    add Network Error

---
+++ Libreria Trantor/System/Network/NetworkError.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkError.swift
+//  Libreria Trantor
+//
+//  Created by Dani Durà on 15/2/23.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case connectionError
+    case responseError
+    case encodingError
+    case authenticationError
+    case timeoutError
+    case URLError
+    case general(Error)
+    
+    var description: String {
+        switch self {
+        case .connectionError:
+            return "connection_error".localized
+        case .responseError:
+            return "response_error".localized
+        case .encodingError:
+            return "encoding_error".localized
+        case .authenticationError:
+            return "authentication_error".localized
+        case .timeoutError:
+            return "timeout_error".localized
+        case .URLError:
+            return "url_error".localized
+        case let .general(error):
+            return "Error de construcción \(error)"
+        }
+    }
+}
