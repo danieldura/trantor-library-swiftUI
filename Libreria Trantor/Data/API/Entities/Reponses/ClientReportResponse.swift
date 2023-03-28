2d0cbb17d8e1ed2df25a3f365de37838aa5f05e1
Dani Durà <danieldura7@gmail.com>
2023-03-28 13:32:58 +0200

    Add clientReport Response

---
+++ Libreria Trantor/Data/API/Entities/Reponses/ClientReportResponse.swift
@@ -0,0 +1,13 @@
+//
+//  ClientReportResponse.swift
+//  DDura Libreria Trantor
+//
+//  Created by Dani Durà on 28/3/23.
+//
+
+import Foundation
+
+struct ClientReportResponse:Codable {
+    var ordered:[Int]?
+    var readed:[Int]?
+}
